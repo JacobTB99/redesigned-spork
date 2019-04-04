7e1b57bc0711a2028bff13396ce061473bc0208c
JacobTB99 <jb467917@ohio.edu>
2019-04-04 13:22:17 -0400

    Creation of file substring.cc. Update of readme. Awaiting code writing for substring.cc.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # redesigned-spork
-A program to count the number of occurences of a substring within a line of text.
+### Author: Jacob Beauchamp
+### Date: April 4, 2019
+---
+A simple program to count the number of occurences of a substring within a line of text.
---
+++ substring.cc
@@ -0,0 +1,37 @@
+/*
+    * Program: substring.cc
+    * Author: Jacob Beauchamp
+    * Date: April 4, 2019
+    * Description: 
+*/
+
+//directives
+#include <iostream>
+#include <string>
+
+//prototypes
+
+
+
+//begin program
+int main(int argc, const char *argv[]) {
+
+}
+
+
+//subsidary definitions
+
+/*
+ *Guidelines:
+ *
+ * Extra credit: Due: Thursday April 4 (11:59 PM).
+ * Write a function that counts the number of occurrences of a substring in a line
+ * of text. The function should work for both upper and lowercase characters.
+ * For example,
+ *           number = countOccurrences("aBC", "ABCdeaBcfgAbcd");
+ * should assign 3 to number.
+ * Write a main program to test your function.
+ * To submit the extra credit, you must create a public repository on GitHub and tag me (@nasseef) when
+ * you are done.
+ *
+*/

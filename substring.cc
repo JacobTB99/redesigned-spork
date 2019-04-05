938ad6a16aa0e14a1a06ad7589977bb5b06eb5ec
JacobTB99 <jb467917@ohio.edu>
2019-04-04 21:31:32 -0400

    Removed guideline comments. @nasseef

--- substring.cc
+++ substring.cc
@@ -88,17 +88,3 @@ void caseMatch(string &string) {
         }
     }
 }
-/*
- *Guidelines:
- *
- * Extra credit: Due: Thursday April 4 (11:59 PM).
- * Write a function that counts the number of occurrences of a substring in a line
- * of text. The function should work for both upper and lowercase characters.
- * For example,
- *           number = countOccurrences("aBC", "ABCdeaBcfgAbcd");
- * should assign 3 to number.
- * Write a main program to test your function.
- * To submit the extra credit, you must create a public repository on GitHub and tag me (@nasseef) when
- * you are done.
- *
-*/
